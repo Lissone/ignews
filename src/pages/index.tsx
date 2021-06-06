1f0aa38e4585849df4731946809cea233d2c5836
Leonardo Dias Lissone <leonardo.lissonez@gmail.com>
2021-06-06 18:35:48 -0300

    change ssr to ssg in home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import { stripe } from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -40,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IzSs5IRbojpBYlsfnq5ZAQ0')
 
   const product = {
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
