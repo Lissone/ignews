78e5a27b636e7a97280504641a21c5ba47fc1625
Leonardo Dias Lissone <leonardo.lissonez@gmail.com>
2021-06-07 18:08:31 -0300

    autentication with github using nextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type='button'
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color='#04D361'/>
 
-      Leonardo Dias
+      {session.user.name}
 
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
@@ -21,6 +23,7 @@ export function SignInButton() {
     <button 
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#EBA417'/>
 
