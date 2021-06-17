976f24189277570ff5519707dfa45647465b1918
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-17 16:22:28 -0300

    save subscriptions in fauna

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -3,6 +3,7 @@ import { Readable } from 'stream'
 import Stripe from 'stripe'
 
 import { stripe } from '../../services/stripe'
+import { saveSubscription } from './_lib/manageSubscription'
 
 // função para concatenar respostas de stream do stripe, para receber todas juntas
 async function buffer(readable: Readable) { 
@@ -52,6 +53,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
       switch (type) {
         case 'checkout.session.completed':
+          const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+          await saveSubscription(
+            checkoutSession.subscription.toString(),
+            checkoutSession.customer.toString()
+          )
+
           break
         default:
           throw new Error('Unhandled event')
