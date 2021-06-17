2efe7284e3b9fb952e73d41cd41fb01c51b7ed65
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-17 15:58:40 -0300

    handling errors without receiving webhooks

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,65 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { Readable } from 'stream'
+import Stripe from 'stripe'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido')
+import { stripe } from '../../services/stripe'
 
-  res.status(200).json({ ok: true })
+// função para concatenar respostas de stream do stripe, para receber todas juntas
+async function buffer(readable: Readable) { 
+  const chunks = []
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    )
+  }
+
+  return Buffer.concat(chunks)
+}
+
+// desabilitar o bodyparser para poder consumir uma stream na req
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
+
+// as respostas dessa função são enviadas para o stripe
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', 'POST')
+    return res.status(405).end('Method not allowed')
+  }
+
+  const buf = await buffer(req)
+  const secret = req.headers['stripe-signature']
+
+  let event: Stripe.Event
+
+  try { // validar se a requisição esta vindo do stripe 
+    event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+  } catch (err) {
+    return res.status(400).send(`Webhook error: ${err.message}`)
+  }
+
+  const { type } = event
+
+  if (relevantEvents.has(type)) {
+    try {
+      switch (type) {
+        case 'checkout.session.completed':
+          break
+        default:
+          throw new Error('Unhandled event')
+      }
+    } catch (err) {
+      return res.json({ error: 'Webhook handler failed' })
+    }
+  }
+
+  res.json({ received: true })
 }
