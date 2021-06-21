a376f51e933b9592fb7587a602d523b8b974cf4f
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-20 21:35:05 -0300

    settings of ssg in posts preview

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { useSession } from 'next-auth/client'
 import { RichText } from 'prismic-dom'
 import Link from 'next/link'
@@ -58,7 +58,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -86,6 +86,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       post
-    }
+    },
+    redirect: 60 * 30 // 30 minutes
   }
 }
